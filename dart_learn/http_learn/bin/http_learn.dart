f325a49bcbb3cea37e47964f6127fb34c47b47fd
black <1358244533@qq.com>
2023-01-01 18:07:24 +0800

    json class

--- dart_learn/http_learn/bin/http_learn.dart
+++ dart_learn/http_learn/bin/http_learn.dart
@@ -1,15 +1,50 @@
+import 'dart:convert';
+
 import 'package:http_learn/http_learn.dart' as http_learn;
 import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) {
   print('Hello world: ${http_learn.calculate()}!');
   sendGetRequest();
-
+  // sendPostRequest();
 }
 
+// void sendGetRequest() async {
+//   final url = Uri.parse("https://baidu.com");
+//   final response = await http.get(url);
+//   print('Response status:${response.statusCode}');
+//   print('Response body:${response.body}');
+// }
+
 void sendGetRequest() async {
-    final url = Uri.parse("https://baidu.com");
-    final response = await http.get(url);
-    print('Response status:${response.statusCode}');
-    print('Response body:${response.body}');
+  final url = Uri.parse("https://jsonplaceholder.typicode.com/albums/1");
+  final response = await http.get(url);
+  if (response.statusCode == 200) {
+    final album = Album.fromJson(jsonDecode(response.body));
+    print('album id:${album.id}, title:${album.title}, userId:${album.userId}');
+  } else {
+    throw Exception('Failed to load album');
   }
+  // print('Response status:${response.statusCode}');
+  // print('Response body:${response.body}');
+}
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  const Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
