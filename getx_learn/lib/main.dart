1952b4fced3ce27b37452d46fa92feb50278dc86
black <1358244533@qq.com>
2023-01-03 11:09:05 +0800

    Get.to

--- getx_learn/lib/main.dart
+++ getx_learn/lib/main.dart
@@ -25,102 +25,135 @@ class Home extends StatelessWidget {
           controller.increment();
         },
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            child: const Text('dialog'),
-            onPressed: () {
-              Get.defaultDialog(
-                title: '标题',
-                titleStyle: const TextStyle(color: Colors.red),
-                content: Column(
-                  children: const [
-                    Text('1'),
-                    Text('2'),
-                    Text('3'),
-                    Text('4'),
-                  ],
-                ),
-                cancel: ElevatedButton(
-                  onPressed: () {
-                    Get.back();
-                  },
-                  child: const Text('取消'),
-                ),
-                confirm: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    // foregroundColor: Colors.white,
-                    backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text('bottomsheet'),
+              onPressed: () {
+                Get.bottomSheet(
+                  Container(
+                    height: 300,
+                    color: Colors.white,
+                    child: ListView(
+                      children: [
+                        ListTile(
+                          leading: const Icon(Icons.restaurant),
+                          trailing: const Icon(Icons.keyboard_arrow_right),
+                          title: const Text('重启'),
+                          onTap: () {
+                            Get.back();
+                          },
+                        ),
+                        const ListTile(
+                          title: Text('注销'),
+                        ),
+                        const ListTile(
+                          title: Text('关机'),
+                        ),
+                      ],
+                    ),
                   ),
-                  onPressed: () {
-                    Get.back();
-                    Get.snackbar('提示', '已确认');
-                  },
-                  child: const Text(
-                    '确认',
-                    style: TextStyle(color: Colors.white),
+                  enableDrag: false,
+                  isDismissible: false,
+                );
+              },
+            ),
+            ElevatedButton(
+              child: const Text('dialog'),
+              onPressed: () {
+                Get.defaultDialog(
+                  title: '标题',
+                  titleStyle: const TextStyle(color: Colors.red),
+                  content: Column(
+                    children: const [
+                      Text('1'),
+                      Text('2'),
+                      Text('3'),
+                      Text('4'),
+                    ],
                   ),
-                ),
-                barrierDismissible: false,
-              );
-            },
-          ),
-          ElevatedButton(
-            child: const Text('snackbar'),
-            onPressed: () {
-              Get.snackbar(
-                '标题', '网络异常',
-                colorText: Colors.white,
-                backgroundColor: Colors.black54,
-                // 显示时间
-                duration: const Duration(seconds: 1),
-                snackPosition: SnackPosition.BOTTOM,
-                titleText: const Text(
-                  '标题',
-                  style: TextStyle(color: Colors.red),
-                ),
-                messageText: Column(
-                  // verticalDirection: VerticalDirection.up,
-                  children: const [
-                    Icon(Icons.add, color: Colors.white),
-                    Icon(Icons.remove, color: Colors.white),
-                    Icon(Icons.list, color: Colors.white),
-                  ],
-                ),
-                icon: const Icon(
-                  Icons.usb_rounded,
-                  color: Colors.white,
-                  size: 35,
-                ),
-                borderWidth: 2.0,
-                borderColor: Colors.red,
-              );
-            },
-          ),
-          GestureDetector(
-            onTap: () {
-              Get.to(Other());
-            },
-            child: Container(
-              height: 200,
-              width: 200,
-              color: Colors.red,
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Obx(
-                  () => Text(
-                    controller.count.toString(),
-                    style: const TextStyle(
-                      fontSize: 50,
-                      color: Colors.white,
+                  cancel: ElevatedButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    child: const Text('取消'),
+                  ),
+                  confirm: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      // foregroundColor: Colors.white,
+                      backgroundColor: Colors.black,
+                    ),
+                    onPressed: () {
+                      Get.back();
+                      Get.snackbar('提示', '已确认');
+                    },
+                    child: const Text(
+                      '确认',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  barrierDismissible: false,
+                );
+              },
+            ),
+            ElevatedButton(
+              child: const Text('snackbar'),
+              onPressed: () {
+                Get.snackbar(
+                  '标题', '网络异常',
+                  colorText: Colors.white,
+                  backgroundColor: Colors.black54,
+                  // 显示时间
+                  duration: const Duration(seconds: 1),
+                  snackPosition: SnackPosition.BOTTOM,
+                  titleText: const Text(
+                    '标题',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  messageText: Column(
+                    // verticalDirection: VerticalDirection.up,
+                    children: const [
+                      Icon(Icons.add, color: Colors.white),
+                      Icon(Icons.remove, color: Colors.white),
+                      Icon(Icons.list, color: Colors.white),
+                    ],
+                  ),
+                  icon: const Icon(
+                    Icons.usb_rounded,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+                  borderWidth: 2.0,
+                  borderColor: Colors.red,
+                );
+              },
+            ),
+            GestureDetector(
+              onTap: () {
+                Get.to(Other(), fullscreenDialog: true, arguments: {'name':'black hole'});
+              },
+              child: Container(
+                height: 200,
+                width: 200,
+                color: Colors.red,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Obx(
+                    () => Text(
+                      controller.count.toString(),
+                      style: const TextStyle(
+                        fontSize: 50,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
@@ -143,7 +176,7 @@ class Other extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: Text("${c.count}"),
+        child: Text("${c.count} - - ${Get.arguments['name']}"),
       ),
     );
   }
